7cdddfe0385aa06a4b88c09af21878b16fbe1c49
GonzaS89 <gnzx89@gmail.com>
2024-10-15 12:17:45 -0300

    asdasd

--- src/Minicomponentes/Servicio.jsx
+++ src/Minicomponentes/Servicio.jsx
@@ -5,7 +5,7 @@ export const Servicio = (props) => {
     <div className="container-servicio">
         <img src={props.img} alt="" className='img-servicio'/>
         <h2>{props.nombre}</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque consectetur minima aliquid laboriosam quidem necessitatibus dolor. Dolorum eligendi, voluptatem delectus dignissimos</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque consectetur minima aliquid laboriosam quidem necessitatibus dolor</p>
     </div>
   )
 }
